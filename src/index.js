ad5fe3eb06ba44bfe1ffc4da93da8b52adc42ae1
Rogerio Chaves <rogeriocfj+github@gmail.com>
2016-12-29 12:14:58 -0200

    Add a way to analyze webpack bundle and split firebase in a separate chunk since it is so large

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 var Elm = require('./app/Main.elm');
-var connectFirebase = require('./firebase');
 
 var app = Elm.Main.embed(document.getElementById('page-wrap'));
 
-connectFirebase(app);
+System.import('./firebase').then(function (connectFirebase) {
+  connectFirebase(app);
+});
