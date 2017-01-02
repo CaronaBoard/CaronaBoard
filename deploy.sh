8982d9a8cff073bf178d7ce46da91cb32176f96c
Rogerio Chaves <rogeriocfj+github@gmail.com>
2017-01-02 12:14:15 -0200

    Back to git url

--- deploy.sh
+++ deploy.sh
@@ -2,16 +2,16 @@ set -e
 
 SHA=`git rev-parse --verify HEAD`
 
-git clone https://github.com/CaronaBoard/caronaboard.github.io
+eval `ssh-agent -s`
+ssh-add /var/snap-ci/repo/id_rsa_caronaboard
+
+git clone git@github.com:CaronaBoard/caronaboard.github.io.git
 rm -rf caronaboard.github.io/**/*
 cp -R build/* caronaboard.github.io
 cd caronaboard.github.io
 
 git config user.name "Snap CI"
 git config user.email "snap-ci@caronaboard.com"
-
-eval `ssh-agent -s`
-ssh-add /var/snap-ci/repo/id_rsa_caronaboard
 
 if [[ -z `git diff --exit-code` ]]; then
     echo "No changes to the output on this push; exiting."
