d93a4d1d0035dc433de6db4f0fefd3819c0ddbef
Rogerio Chaves <rogeriocfj+github@gmail.com>
2017-01-02 12:53:17 -0200

    Fix checking for no changes

--- deploy.sh
+++ deploy.sh
@@ -13,10 +13,7 @@ cd caronaboard.github.io
 git config user.name "Snap CI"
 git config user.email "snap-ci@caronaboard.com"
 
-if [[ -z `git diff --exit-code` ]]; then
-    echo "No changes to the output on this push; exiting."
-    exit 0
-fi
+git diff --exit-code && echo "No changes to the output on this push; exiting." && exit 0
 
 git add -A
 git commit -m "Deploy to GitHub Pages: ${SHA}"
