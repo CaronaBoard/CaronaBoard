bd7db0b719c6f7939a53cf381b2d14746d864b86
Rogerio Chaves <rogeriocfj@gmail.com>
2017-02-19 10:47:40 -0300

    Fix id_rsa decryption and usage

--- deploy.sh
+++ deploy.sh
@@ -2,10 +2,13 @@ set -e
 
 SHA=`git rev-parse --verify HEAD`
 
+openssl aes-256-cbc -K $encrypted_6d2f7c519862_key -iv $encrypted_6d2f7c519862_iv -in id_rsa_caronaboard.enc -out id_rsa_caronaboard -d
+chmod 400 id_rsa_caronaboard
+
 eval `ssh-agent -s`
 ssh-add id_rsa_caronaboard
 
-git clone git@github.com:CaronaBoard/caronaboard.github.io.git
+git clone git@github.com:CaronaBoard/caronaboard.github.io.git caronaboard.github.io
 rm -rf caronaboard.github.io/**/*
 cp -R build/* caronaboard.github.io
 cd caronaboard.github.io
