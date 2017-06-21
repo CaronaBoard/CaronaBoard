ae4ea7a4128d6a8868386c55b893ea1e5244b592
Rogerio Chaves <rogeriocfj@gmail.com>
2017-06-21 13:51:41 -0300

    Replace travis with circle-ci

--- deploy.sh
+++ deploy.sh
@@ -2,12 +2,6 @@ set -e
 
 SHA=`git rev-parse --verify HEAD`
 
-openssl aes-256-cbc -K $encrypted_6d2f7c519862_key -iv $encrypted_6d2f7c519862_iv -in id_rsa_caronaboard.enc -out id_rsa_caronaboard -d
-chmod 400 id_rsa_caronaboard
-
-eval `ssh-agent -s`
-ssh-add id_rsa_caronaboard
-
 git clone git@github.com:CaronaBoard/caronaboard.github.io.git caronaboard.github.io
 rm -rf caronaboard.github.io/*
 rm -rf caronaboard.github.io/**/*
@@ -17,8 +11,8 @@ cd caronaboard.github.io
 echo "caronaboard.com" >> CNAME
 echo "www.caronaboard.com" >> CNAME
 
-git config user.name "Travis CI"
-git config user.email "travis-ci@caronaboard.com"
+git config user.name "Circle CI"
+git config user.email "circle-ci@caronaboard.com"
 
 git add -A
 git commit -m "Deploy to GitHub Pages: ${SHA}" || echo "nothing to commit"
