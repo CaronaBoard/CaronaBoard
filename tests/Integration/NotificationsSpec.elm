89a45ffca5f6f1424f29bcb2135d330da27df570
Rogerio Chaves <rogeriocfj@gmail.com>
2017-06-25 00:49:56 -0300

    Redirect to rides page after enabling notifications

--- tests/Integration/NotificationsSpec.elm
+++ tests/Integration/NotificationsSpec.elm
@@ -4,6 +4,7 @@ import Expect exposing (equal)
 import Helpers exposing (expectToContainText, initialContext, someUser, toLocation)
 import Model exposing (Model)
 import Msg as Root exposing (Msg(..))
+import Navigation
 import Notifications.Msg exposing (..)
 import Notifications.Ports
 import Test exposing (..)
@@ -27,6 +28,10 @@ tests =
                 >> update (MsgForNotifications <| NotificationsResponse ( Just "I don't like notifications", Nothing ))
                 >> find []
                 >> assertText (expectToContainText "I don't like notifications")
+        , test "goes to the rides page on success" <|
+            enableNotifications
+                >> update (MsgForNotifications <| NotificationsResponse ( Nothing, Just () ))
+                >> assertCalled (Cmd.map MsgForUrlRouter <| Navigation.newUrl <| toPath RidesPage)
         ]
 
 
