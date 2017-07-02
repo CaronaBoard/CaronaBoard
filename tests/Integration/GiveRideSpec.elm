79523f01d86024bb7abfb6a35ebaf3450168bbd8
Rogerio Chaves <rogeriocfj@gmail.com>
2017-07-02 18:08:30 -0300

    Fixed tests that involves navigation for GiveRide

--- tests/Integration/GiveRideSpec.elm
+++ tests/Integration/GiveRideSpec.elm
@@ -1,11 +1,11 @@
 module Integration.GiveRideSpec exposing (tests)
 
 import Common.Response exposing (Response(..))
+import Expect
 import GiveRide.Model exposing (Msg(..))
 import GiveRide.Ports
 import Helpers exposing (expectToContainText, fixtures, initialContext, signedInContext, someUser, toLocation)
 import Model as Root exposing (Model, Msg(..))
-import Navigation
 import Notifications.Model exposing (Msg(..))
 import Test exposing (..)
 import Test.Html.Events as Events exposing (Event(..))
@@ -36,27 +36,33 @@ tests =
                 >> update (MsgForGiveRide <| GiveRideResponse (Error "Scientists just proved that undefined is indeed not a function"))
                 >> expectView
                 >> has [ text "not a function" ]
-
-        -- , test "goes to enable notifications page on success" <|
-        --     submitNewRide
-        --         >> successResponse
-        --         >> expectCmd (Cmd.map MsgForUrlRouter <| Navigation.newUrl <| toPath EnableNotificationsPage)
-        -- , test "goes to the rides page on success if notifications are already enabled" <|
-        --     submitNewRide
-        --         >> update (MsgForNotifications <| NotificationsResponse (Success True))
-        --         >> successResponse
-        --         >> expectCmd (Cmd.map MsgForUrlRouter <| Navigation.newUrl <| toPath RidesPage)
-        -- , test "shows notification on success" <|
-        --     submitNewRide
-        --         >> successResponse
-        --         >> expectView
-        --         >> has [ text "Carona criada com sucesso!" ]
-        -- , test "clear fields on success" <|
-        --     submitNewRide
-        --         >> successResponse
-        --         >> expectView
-        --         >> find [ id "origin" ]
-        --         >> has [ attribute "value" "" ]
+        , test "goes to enable notifications page on success" <|
+            submitNewRide
+                >> successResponse
+                >> expectModel
+                    (\model ->
+                        Expect.equal EnableNotificationsPage model.urlRouter.page
+                    )
+        , test "goes to the rides page on success if notifications are already enabled" <|
+            submitNewRide
+                >> update (MsgForNotifications <| NotificationsResponse (Success True))
+                >> successResponse
+                >> expectModel
+                    (\model ->
+                        Expect.equal RidesPage model.urlRouter.page
+                    )
+        , test "shows notification on success" <|
+            submitNewRide
+                >> successResponse
+                >> expectView
+                >> has [ text "Carona criada com sucesso!" ]
+        , test "clear fields on success after returning to the form" <|
+            submitNewRide
+                >> successResponse
+                >> navigate (toPath GiveRidePage)
+                >> expectView
+                >> find [ id "origin" ]
+                >> has [ attribute "value" "" ]
         ]
 
 
