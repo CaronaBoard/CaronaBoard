44267313aac4a2b8bb83d38faeb5cce3a1ea28f7
Rogerio Chaves <rogeriocfj@gmail.com>
2017-07-04 21:11:02 -0400

    Goes to rides page when selecting a group

--- src/app/Groups/View.elm
+++ src/app/Groups/View.elm
@@ -5,10 +5,15 @@ import Common.Response exposing (Response(..))
 import Groups.Model exposing (Group, Model)
 import Groups.Styles exposing (Classes(..), className)
 import Html exposing (..)
+import Html.Attributes exposing (id)
+import Html.Events exposing (onClick)
 import Layout.Styles exposing (Classes(..), layoutClass)
+import Model as Root exposing (Msg(..))
+import UrlRouter.Model exposing (Msg(..))
+import UrlRouter.Routes exposing (Page(..))
 
 
-groupsList : Model -> Html msg
+groupsList : Model -> Html Root.Msg
 groupsList model =
     case model.groups of
         Empty ->
@@ -29,6 +34,6 @@ groupsList model =
             text err
 
 
-groupItem : Group -> Html msg
+groupItem : Group -> Html Root.Msg
 groupItem group =
-    li [] [ text group.name ]
+    li [ id group.id, onClick (MsgForUrlRouter <| Go <| RidesPage group.id) ] [ text group.name ]
--- tests/Integration/GroupsSpec.elm
+++ tests/Integration/GroupsSpec.elm
@@ -6,6 +6,7 @@ import Helpers exposing (expectToContainText, fixtures, initialContext, jsonQuot
 import JsonStringify exposing (simpleStringify)
 import Model as Root exposing (Model, Msg(..))
 import Test exposing (..)
+import Test.Html.Events as Events
 import Test.Html.Query exposing (..)
 import Test.Html.Selector exposing (..)
 import TestContext exposing (..)
@@ -24,13 +25,26 @@ tests =
                 >> has [ text "Carregando..." ]
         , test "renders groups when they load" <|
             groupsContext
-                >> send groupsListResponse ( Nothing, Just <| simpleStringify { idGroup1 = fixtures.group1, idGroup2 = fixtures.group2 } )
+                >> loadGroups
                 >> expectView
                 >> Expect.all
                     [ has [ tag "li", text "winona riders" ]
                     , has [ tag "li", text "the uber killars" ]
                     ]
+        , test "goes the the group's rides when clicking on it" <|
+            groupsContext
+                >> loadGroups
+                >> simulate (find [ id "idGroup1" ]) Events.Click
+                >> expectModel
+                    (\model ->
+                        Expect.equal (RidesPage "idGroup1") model.urlRouter.page
+                    )
         ]
+
+
+loadGroups : TestContext model msg -> TestContext model msg
+loadGroups =
+    send groupsListResponse ( Nothing, Just <| simpleStringify { idGroup1 = fixtures.group1, idGroup2 = fixtures.group2 } )
 
 
 groupsContext : a -> TestContext Model Root.Msg
