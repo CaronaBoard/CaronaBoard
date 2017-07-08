b916393a4491ae7b3416f8dc9d2559bcf77dcf11
Rogerio Chaves <rogeriocfj@gmail.com>
2017-07-08 13:42:57 -0400

    Load material icons directly and remove custom font, all to improve loading speed even further

--- src/index.js
+++ src/index.js
@@ -1,10 +1,5 @@
 // After splash screen loaded
 window.addEventListener("load", function() {
-  // Load all CSS
-  var loadCSS = require("fg-loadcss").loadCSS;
-  loadCSS("https://fonts.googleapis.com/icon?family=Material+Icons");
-  loadCSS("https://fonts.googleapis.com/css?family=Lato:400,700");
-
   // Load Elm and connect it with Firebase
   require("./js/elm.js");
 
