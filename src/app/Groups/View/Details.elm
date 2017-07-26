75834fa5586cc066c02371b3b9922cb78bdadad0
Rogerio Chaves <rogeriocfj@gmail.com>
2017-07-26 09:17:11 -0300

    Add more styling and loading behaviours to group details page

--- src/app/Groups/View/Details.elm
+++ src/app/Groups/View/Details.elm
@@ -1,10 +1,31 @@
 module Groups.View.Details exposing (details)
 
-import Groups.Model exposing (Model)
+import Common.IdentifiedList exposing (findById)
+import Common.Response as Response exposing (Response(..))
 import Html exposing (..)
+import Layout.Styles exposing (Classes(..), layoutClass)
 import Model as Root exposing (Msg(..))
 
 
-details : Model -> Html Root.Msg
-details model =
-    text "Você não faz parte desse grupo. [Participar do grupo]"
+details : String -> Root.Model -> Html Msg
+details groupId { groups } =
+    case groups.groups of
+        Empty ->
+            text "TODO: fetch groups"
+
+        Loading ->
+            text "Carregando..."
+
+        Success groups ->
+            case findById groupId groups of
+                Just group ->
+                    div [ layoutClass Container ]
+                        [ h1 [ layoutClass PageTitle ] [ text group.name ]
+                        , text "Você não faz parte desse grupo. [Participar do grupo]"
+                        ]
+
+                Nothing ->
+                    h1 [] [ text "404 não encontrado" ]
+
+        Error err ->
+            text err
