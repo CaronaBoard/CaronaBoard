8ea48e4aa669f72335f35b03424f9a7bcaafba0a
Rogerio Chaves <rogeriocfj@gmail.com>
2017-08-02 22:21:42 -0300

    Improve ui for requesting to join group

--- src/app/Groups/View/Details.elm
+++ src/app/Groups/View/Details.elm
@@ -1,10 +1,11 @@
 module Groups.View.Details exposing (details)
 
+import Common.Form exposing (loadingOrSubmitButton)
 import Common.IdentifiedList exposing (findById)
 import Common.Response as Response exposing (Response(..))
 import Groups.Model exposing (Msg(..))
 import Html exposing (..)
-import Html.Attributes exposing (..)
+import Html.Attributes exposing (disabled)
 import Html.Events exposing (..)
 import Layout.Styles exposing (Classes(..), layoutClass)
 import Model as Root exposing (Msg(..))
@@ -35,22 +36,15 @@ renderGroup : Groups.Model.Group -> Html Root.Msg
 renderGroup group =
     div [ layoutClass Container ]
         [ h1 [ layoutClass PageTitle ] [ text group.name ]
-        , text "Você não faz parte desse grupo."
-        , button
-            [ id "joinGroup"
-            , onClick <| MsgForGroups <| CreateJoinGroupRequest group.id
+        , form [ layoutClass Card, onSubmit (MsgForGroups <| CreateJoinGroupRequest group.id) ]
+            [ p [] [ text "Este grupo é um grupo fechado, clique abaixo para pedir autorização dos administradores para entrar no grupo." ]
+            , br [] []
+            , case group.joinRequest of
+                Success _ ->
+                    button [ disabled True, layoutClass DisabledButton ]
+                        [ div [ layoutClass ButtonContainer ] [ text "Pedido enviado!" ] ]
+
+                _ ->
+                    loadingOrSubmitButton group.joinRequest "joinGroup" [ text "Participar do grupo" ]
             ]
-            [ text "Participar do grupo" ]
-        , case group.joinRequest of
-            Empty ->
-                text ""
-
-            Loading ->
-                text "Carregando..."
-
-            Success _ ->
-                text "Pedido enviado!"
-
-            Error err ->
-                text err
         ]
