05a192cf857647db8dbb58f8044c782da4ab7362
Rogerio Chaves <rogeriocfj@gmail.com>
2017-08-02 22:47:32 -0300

    Add javascript part for creating join group request

--- src/js/firebase/groups.js
+++ src/js/firebase/groups.js
@@ -2,6 +2,8 @@ var success = require("./helpers").success;
 var error = require("./helpers").error;
 
 module.exports = function(firebase, app) {
+  var getProfile = require("./profile").getProfile(firebase, app);
+
   app.ports.groupsList.subscribe(function() {
     firebase
       .database()
@@ -12,6 +14,33 @@ module.exports = function(firebase, app) {
       })
       .catch(function(err) {
         app.ports.groupsListResponse.send(error(err.message));
+      });
+  });
+
+  app.ports.createJoinGroupRequest.subscribe(function(joinGroupRequest) {
+    getProfile()
+      .then(function(profile) {
+        return firebase
+          .database()
+          .ref(
+            "joinGroupRequests/" +
+              joinGroupRequest.groupId +
+              "/" +
+              firebase.auth().currentUser.uid
+          )
+          .set({ profile: profile.val() });
+      })
+      .then(function() {
+        app.ports.createJoinGroupRequestResponse.send({
+          groupId: joinGroupRequest.groupId,
+          response: success(true)
+        });
+      })
+      .catch(function(err) {
+        app.ports.createJoinGroupRequestResponse.send({
+          groupId: joinGroupRequest.groupId,
+          response: error(err.message)
+        });
       });
   });
 };
