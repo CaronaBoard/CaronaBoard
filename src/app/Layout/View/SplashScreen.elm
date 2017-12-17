6e3a65a085b68f31121a5b3319f5aca1dff7a41b
Rogerio Chaves <rogeriocfj@gmail.com>
2017-12-17 19:24:37 -0200

    Migrate splashScreen

--- src/app/Layout/View/SplashScreen.elm
+++ src/app/Layout/View/SplashScreen.elm
@@ -2,41 +2,36 @@ module Layout.View.SplashScreen exposing (splashScreen)
 
 import Common.Colors exposing (..)
 import Css exposing (..)
-import DEPRECATED.Css exposing (asPairs)
-import Html exposing (Html, div, img)
-import Html.Attributes exposing (src, style)
+import Html
+import Html.Styled exposing (Html, div, img, toUnstyled)
+import Html.Styled.Attributes exposing (css, src)
 import Model exposing (Msg(..))
 
 
-splashScreen : Html Msg
+splashScreen : Html.Html Msg
 splashScreen =
-    div [ style splashScreenStyle ]
-        [ img
-            [ Html.Attributes.src "static/images/logo.svg"
-            , style splashIconStyle
-            ]
-            []
+    div [ css splashScreenStyle ]
+        [ img [ src "static/images/logo.svg", css splashIconStyle ] []
         ]
+        |> toUnstyled
 
 
-splashScreenStyle : List ( String, String )
+splashScreenStyle : List Style
 splashScreenStyle =
-    asPairs
-        [ width (pct 100)
-        , height (pct 100)
-        , top (px 0)
-        , left (px 0)
-        , position fixed
-        , backgroundColor primaryBlue
-        , displayFlex
-        , justifyContent center
-        , alignItems center
-        ]
+    [ width (pct 100)
+    , height (pct 100)
+    , top (px 0)
+    , left (px 0)
+    , position fixed
+    , backgroundColor primaryBlue
+    , displayFlex
+    , justifyContent center
+    , alignItems center
+    ]
 
 
-splashIconStyle : List ( String, String )
+splashIconStyle : List Style
 splashIconStyle =
-    asPairs
-        [ width (px 192)
-        , height (px 192)
-        ]
+    [ width (px 192)
+    , height (px 192)
+    ]
